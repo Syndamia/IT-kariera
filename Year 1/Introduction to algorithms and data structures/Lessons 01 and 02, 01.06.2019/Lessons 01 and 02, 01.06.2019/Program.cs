ac0f6b44b74fe106fa500f1b8d4e4ea9e7051326
Syndamia <kami02882@gmail.com>
2019-06-07 13:14:39 +0300

    Added all exercises from lesson 1 and created a foldr for this course

---
+++ Year 1/Introduction to algorithms and data structures/Lessons 01 and 02, 01.06.2019/Lessons 01 and 02, 01.06.2019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_01_and_02__01._06._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
