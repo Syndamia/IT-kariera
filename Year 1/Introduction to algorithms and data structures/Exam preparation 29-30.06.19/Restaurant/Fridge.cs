a54bfbbe401a809e218c62dddca888177aeefd1e
Syndamia <kami02882@gmail.com>
2019-07-01 19:28:04 +0300

    Added exam preparation task Products

---
+++ Year 1/Introduction to algorithms and data structures/Exam preparation 29-30.06.19/Restaurant/Fridge.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant {
+    class Fridge {
+        private Product head;
+        private Product tail;
+        private int count;
+
+        public Fridge() { }
+
+        public int Count {
+            get { return count; }
+            private set { count = value; }
+        }
+
+        public void AddProduct(string ProductName) {
+            var newProduct = new Product(ProductName);
+
+            if (head == null) {
+                head = newProduct;
+                tail = newProduct;
+            }
+            else {
+                tail.Next = newProduct;
+                tail = newProduct;
+            }
+
+            Count++;
+        }
+
+        public string[] CookMeal(int start, int end) {
+            var currP = head;
+            var mealProducts = new List<string>();
+            
+            for (int i = 0; i < Count && currP != null; i++) {
+                if (i >= start && i <= end) {
+                    mealProducts.Add(currP.Name);
+                }
+                currP = currP.Next;
+            }
+
+            return mealProducts.ToArray(); 
+        }
+
+        public string RemoveProductByIndex(int index) {
+            if(index < 0 || index >= Count) return null;
+            else {
+                var currP = head;
+                string toReturn = null;
+
+                if (index == 0) {
+                    toReturn = currP.Name;
+                    head = head.Next;
+                }
+                else {
+                    for(int i = 0; i < index; i++) {
+                        if(i == index - 1) {
+                            toReturn = currP.Next.Name;
+
+                            if(currP.Next == tail) {
+                                tail = currP;
+                                tail.Next = null;
+                            }
+                            else currP.Next = currP.Next.Next;
+                            break;
+                        }
+
+                        currP = currP.Next;
+                    }
+                }
+
+                Count--;
+                return toReturn;
+            }
+        }
+
+        public string RemoveProductByName(string name) {
+            var currP = head;
+
+            for(int i = 0; i < Count; i++) {
+                if(currP.Name == name) return RemoveProductByIndex(i);
+                currP = currP.Next;
+            }
+
+            return null;
+        }
+
+        public bool CheckProductIsInStock(string name) {
+            var currP = head;
+
+            while(currP != null) {
+                if(currP.Name == name) return true;
+
+                currP = currP.Next;
+            }
+
+            return false;
+        }
+
+        public string[] ProvideInformationAboutAllProducts() {
+            var currP = head;
+            var info = new string[Count];
+
+            for (int i = 0; i < Count; i++) {
+                info[i] = currP.ToString();
+                currP = currP.Next;
+            }
+
+            return info;
+        }
+
+        private string DealWithNext(Product currP) {
+            
+
+            return currP.Name;
+        }
+    }
+}
---
+++ Year 1/Introduction to algorithms and data structures/Exam preparation 29-30.06.19/Restaurant/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant {
+    class Product {
+        private string name;
+        private Product next;
+
+        public Product(string name) {
+            Name = name;
+        }
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Product Next {
+            get { return next; }
+            set { next = value; }
+        }
+
+        public override string ToString() { 
+            return $"Product {Name}";
+        }
+    }
+}
