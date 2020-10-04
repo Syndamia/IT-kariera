8bd620d525e588d76f47602efcf30ae0c26c093e
Syndamia <kamen.d.mladenov@protonmail.com>
2020-10-04 22:17:32 +0300

    Added exam solutions (plus description) of module 9 final exam

---
+++ Year 3/Algorithms and data structures/Exam 04.10.2020/ATM/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ATM {
+    class MainClass {
+        public static void Main(string[] args) {
+            int sum = int.Parse(Console.ReadLine());
+
+            int banknotes = 0;
+
+            if (sum / 100 > 0) {
+                banknotes += sum / 100;
+                sum %= 100;
+            }
+            if (sum / 50 > 0) {
+                banknotes += sum / 50;
+                sum %= 50;
+            }
+            if (sum / 20 > 0) {
+                banknotes += sum / 20;
+                sum %= 20;
+            }
+            if (sum / 10 > 0) {
+                banknotes += sum / 10;
+                sum %= 10;
+            }
+            if (sum / 5 > 0) {
+                banknotes += sum / 5;
+                sum %= 5;
+            }
+            if (sum / 2 > 0) {
+                banknotes += sum / 2;
+                sum %= 2;
+            }
+            if (sum > 0) {
+                banknotes += sum;
+            }
+
+            Console.WriteLine(banknotes);
+        }
+    }
+}
---
+++ Year 3/Algorithms and data structures/Exam 04.10.2020/Permutations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Permutations {
+    class MainClass {
+        public static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] elements = new int[n];
+            for (int i = 1; i <= n; i++) {
+                elements[i - 1] = i;
+            }
+
+            Permute(0, elements, new int[n], new bool[n]);
+        }
+
+        public static void Permute(int index, int[] elements, int[] permuted, bool[] used) {
+            if (index >= elements.Length) {
+                // Not using string.Join, because the system wouldn't give some points
+                foreach (var num in permuted) {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++) {
+                if (!used[i]) {
+                    used[i] = true;
+                    permuted[index] = elements[i];
+                    Permute(index + 1, elements, permuted, used);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
