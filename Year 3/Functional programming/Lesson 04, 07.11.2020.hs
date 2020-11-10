b3f1d26a5946fc70e8925677e7e37ec7a4bbc21f
Syndamia <kamen.d.mladenov@protonmail.com>
2020-11-10 12:08:46 +0200

    Finished haskell exercises from lesson 4

--- Year 3/Functional programming/Lesson 04, 07.11.2020.hs
+++ Year 3/Functional programming/Lesson 04, 07.11.2020.hs
@@ -55,6 +55,32 @@ takeElement arr index start end = if start <= index && index <= end then [arr!!i
 subElements :: [a] -> Int -> Int -> [a]
 subElements arr start end = foldl (\array index -> array ++ takeElement arr index start end) [] [0..(length arr - 1)]
 
+{- 06. Closures -}
+
+-- Ex. 1
+
+multiply :: Int -> Int -> Int
+multiply a = \b -> a * b
+
+-- Ex. 2
+
+biggestOfThree :: Int -> Int -> Int -> Int
+biggestOfThree a = \b -> \c -> if a > b && a > c then a
+                               else if b > a && b > c then b
+                               else c
+
+-- Ex. 3
+
+functionArg :: (a -> a) -> a -> a
+functionArg func = \a -> func a
+
+-- Ex. 4
+
+mathEq1 :: [Int] -> String
+mathEq1 [] = ""
+mathEq1 nums = foldl (\str a -> "(" ++ str ++ "+" ++ show a ++ ")") (show (head nums)) (tail nums)
+
+
 main :: IO()
 main = do
     print (mathEq [1, 2, 3, 4, 5])
@@ -63,3 +89,8 @@ main = do
     print (duplicateElem [1, 2, 3, 4, 4])
     print (duplicateElemTimes [1, 2, 3] 3)
     print (subElements [1, 2, 3, 4, 5] 2 3)
+
+    print (multiply 5 10)
+    print (biggestOfThree 5 10 (-20))
+    print (functionArg (\x -> x + 1) 5)
+    print (mathEq1 [1, 2, 3, 4, 5])
