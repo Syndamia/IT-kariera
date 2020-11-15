25e10956ad5e178e20e444798abb91522f44634e
Syndamia <kamen.d.mladenov@protonmail.com>
2020-11-15 16:52:59 +0200

    Added final module 10 exam preparation exercise solution

---
+++ Year 3/Functional programming/Exam preparation/LINQuistics.hs
@@ -0,0 +1,131 @@
+-- Third exercise from Lists-Advanced-Exercises.docx
+
+{- Split -}
+
+splitAtSection :: String -> Int -> [Int]
+splitAtSection str start = if start == length str    then [start, 0]
+                           else if str!!start == '.' then [start, 1]
+                           else if str!!start == '(' then [start, 3]
+                           else splitAtSection str (start + 1)
+
+splitInput :: String -> [String]
+splitInput line = if null line then []
+                  else [take (head (splitAtSection line 0)) line] ++ splitInput (drop (sum (splitAtSection line 0)) line)
+
+indexOfHead :: [[String]] -> String -> Int -> Int
+indexOfHead mat key start = if null mat then (-1)
+                            else if head (head mat) == key then start
+                            else indexOfHead (tail mat) key (start + 1)
+
+{- Insert -}
+
+insertInfo :: [[String]] -> Int -> [String] -> [[String]]
+insertInfo mat index info = take index mat ++ [mat!!index ++ info] ++ drop (index + 1) mat
+
+insertIncl :: [[String]] -> [String] -> [[String]]
+insertIncl mat info = if indexOfHead mat (head info) 0 >= 0 then
+                          insertInfo mat (indexOfHead mat (head info) 0) (tail info)
+                      else mat ++ [info]
+
+{- Sort -}
+
+swapTwo :: [a] -> Int -> [a]
+swapTwo arr i = take i arr ++ [arr!!(i + 1)] ++ [arr!!i] ++ drop (i + 2) arr
+
+desc :: (Ord a) => [a] -> (a -> a -> Bool) -> (a -> a -> Bool) ->  Int -> Int -> [a]
+desc arr cond scond end curr = if end == 0 then arr
+                               else if curr == end then desc arr cond scond (end - 1) 0
+                               else if cond (arr!!curr) (arr!!(curr + 1)) || scond (arr!!curr) (arr!!(curr+1)) 
+                                   then desc (swapTwo arr curr) cond scond end (curr + 1)
+                               else desc arr cond scond end (curr + 1)
+
+orderByDesc :: (Ord a) => [a] -> (a -> a -> Bool) -> (a -> a -> Bool) -> [a]
+orderByDesc arr cond scond = desc arr cond scond (length arr - 1) 0
+
+{- Sort conditions -}
+
+compByUniqCnt :: String -> String -> Bool
+compByUniqCnt x y = length x == length y && uniqCnt x <= uniqCnt y  
+    where uniqCnt str = length (foldl (\acc x -> if elem x acc then acc else acc ++ [x]) [] str)
+
+compByLen :: (Ord a) => [a] -> [a] -> Bool
+compByLen x y = length x < length y
+
+compByShort :: (Ord a) => [a] -> [a] -> Bool
+compByShort x y = length x > length y
+
+alwaysFalse :: a -> a -> Bool
+alwaysFalse x y = False
+
+{- Fiters -}
+
+elemsThatContain :: [[String]] -> String -> [[String]]
+elemsThatContain mat item = if null mat then []
+                            else if elem item (head mat) then [head mat] ++ elemsThatContain (tail mat) item
+                            else elemsThatContain (tail mat) item
+
+getCollWithMostMethods :: [[String]] -> [String]
+getCollWithMostMethods mat = mat!!(indexOfMostMethods)
+    where indexOfMostMethods = foldl (\acc x -> if lengthsArr!!x > lengthsArr!!acc then x else acc) 0 [1..(length mat - 1)] 
+           where lengthsArr = map (\x -> length x) mat 
+
+{- Printing -}
+
+printMethods arr =
+    if null arr then return ()
+    else do
+        putStrLn ("* " ++ (head arr))
+        printMethods (tail arr)
+
+printCollection :: [[String]] -> Bool -> IO ()
+printCollection collection haveMethods = do
+    if null collection then return ()
+    else do
+        putStrLn (head (head collection))
+        if haveMethods then do
+            let ordMethods = orderByDesc (tail (head collection)) compByLen alwaysFalse 
+            printMethods ordMethods
+            printCollection (tail collection) haveMethods
+        else printCollection (tail collection) haveMethods
+
+{- Main -}
+
+isNumber :: String -> Bool
+isNumber str = if null str then False
+               else if length str == 1 then 
+                    head str >= '0' && head str <= '9'
+               else head str >= '0' && head str <= '9' && isNumber (tail str)
+
+readUntilWord :: [[String]] -> IO ()
+readUntilWord mat = do
+    line <- getLine
+    let coll = splitInput line
+
+    if length coll == 1 then 
+        if head coll == "exit" then do
+            final <- getLine
+            let comm = words final
+            let collecs = elemsThatContain mat (head comm)
+            let ordered = orderByDesc collecs compByLen alwaysFalse
+            
+            printCollection ordered (comm!!1 == "all")
+        
+        else if isNumber (head coll) then do
+            let mostMethodsOrd = orderByDesc (tail (getCollWithMostMethods mat)) compByShort alwaysFalse
+            let n = read (head coll) :: Int
+
+            printMethods (take n mostMethodsOrd)
+            readUntilWord mat
+
+        else if indexOfHead mat (head coll) 0 >= 0 then do
+            let methods = orderByDesc (tail (mat!!(indexOfHead mat (head coll) 0))) compByLen compByUniqCnt 
+            printMethods methods
+            readUntilWord mat
+
+        else readUntilWord mat    
+
+    else readUntilWord (insertIncl mat coll)
+
+main :: IO()
+main = do
+    readUntilWord []
